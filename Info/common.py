4dec422adc98cf6e68a33f5cf120dbe3153f8a63
Allcen <1310584051@qq.com>
2018-08-26 17:18:35 +0800

    新闻详细页面其他部分展示(以及根据登陆情况显示评论框)

--- Info/common.py
+++ Info/common.py
@@ -1,4 +1,11 @@
 # 定义索引转换过滤器(如果使用装饰器的方法定义过滤器，这里先得使用current_app进行关联，init文件中也要声明这个装饰器，相比很麻烦)
+import functools
+
+from flask import session, current_app, g
+
+from Info.models import User
+
+
 def index_convert(index):
     index_dict = {
         1: "first",
@@ -6,3 +13,27 @@ def index_convert(index):
         3: "third"
     }
     return index_dict.get(index, "")
+
+
+# 查询用户登陆状态
+def user_login_data(f):
+    @functools.wraps(f)  # 可以让闭包函数wrapper使用指定函数f的函数信息（如函数名__name__,文档注释__doc__）
+    def wrapper(*args, **kwargs):
+        # 判断用户是否登陆
+        user_id = session.get("user_id")
+        user = None  # 当某些极端情况下，user_id没有值，这样从库里就取不出数据，但是模板渲染里仍要传值，这时需要相当于对user进行初始化
+        if user_id:
+            # 根据user_id查询用户模型
+            try:
+                user = User.query.get(user_id)
+            except Exception as e:
+                current_app.logger.error(e)
+
+        # user = user.to_dict() if user else None  # 因为这里只是用于验证用户是否登陆，不需要对user里的取值进行格式化
+
+        g.user = user  # 让g变量记录查询出的用户数据
+
+        # 再执行原有的功能
+        return f(*args, **kwargs)
+
+    return wrapper
--- Info/modules/news/views.py
+++ Info/modules/news/views.py
@@ -1,11 +1,14 @@
-from flask import current_app, abort, render_template
+from flask import current_app, abort, render_template, g
 
+from Info.common import user_login_data
+from Info.constants import CLICK_RANK_MAX_NEWS
 from Info.models import News
 from Info.modules.news import news_blu
 
 
 # 显示新闻详情页
 @news_blu.route('/<int:news_id>')
+@user_login_data
 def news_detail(news_id):
     # 根据新闻id来查询该新闻模型、
     news = None  # type:News
@@ -19,5 +22,18 @@ def news_detail(news_id):
 
     # 让新闻的点击量+1
     news.clicks += 1
+
+    # 查询新闻 按照点击量的倒序排列 取前10条
+    rank_list = []
+    try:
+        rank_list = News.query.order_by(News.clicks.desc()).limit(CLICK_RANK_MAX_NEWS).all()
+    except Exception as e:
+        current_app.logger.error(e)
+
+    rank_list = [news.to_dict() for news in rank_list]
+
+    # 将用户登陆信息传到模板中
+    user = g.user.to_dict() if g.user else None
+
     # 将模型数据传到模板中
-    return render_template("news/detail.html", news=news.to_dict())
+    return render_template("news/detail.html", news=news.to_dict(), rank_list=rank_list, user=user)
