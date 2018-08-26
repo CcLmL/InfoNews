0e016fcc9dc49e40bcda09a30526c673f07e5fba
Allcen <1310584051@qq.com>
2018-08-26 19:31:37 +0800

    显示收藏情况

--- Info/modules/news/views.py
+++ Info/modules/news/views.py
@@ -1,4 +1,4 @@
-from flask import current_app, abort, render_template, g
+from flask import current_app, abort, render_template, g, jsonify, request
 
 from Info.common import user_login_data
 from Info.constants import CLICK_RANK_MAX_NEWS
@@ -7,6 +7,9 @@ from Info.modules.news import news_blu
 
 
 # 显示新闻详情页
+from Info.utils.response_code import RET, error_map
+
+
 @news_blu.route('/<int:news_id>')
 @user_login_data
 def news_detail(news_id):
@@ -32,8 +35,60 @@ def news_detail(news_id):
 
     rank_list = [news.to_dict() for news in rank_list]
 
+    # 查询当前用户是否收藏了该新闻
+    is_collected = False
+    user = g.user
+    if user:
+        if news in user.collection_news:
+            is_collected = True
+
     # 将用户登陆信息传到模板中
-    user = g.user.to_dict() if g.user else None
+    user = user.to_dict() if user else None
 
     # 将模型数据传到模板中
-    return render_template("news/detail.html", news=news.to_dict(), rank_list=rank_list, user=user)
+    return render_template("news/detail.html", news=news.to_dict(), rank_list=rank_list, user=user,
+                           is_collected=is_collected)
+
+
+# 收藏/取消收藏
+@news_blu.route('/news_collect',methods=["POST"])
+@user_login_data
+def news_collect():
+    # 判断用户是否登陆
+    user = g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg=error_map[RET.SESSIONERR])
+    # 获取参数
+    news_id = request.json.get("news_id")
+    action = request.json.get("action")
+    # 校验参数
+    if not all([news_id, action]):
+        return jsonify(errno=RET.PARAMERR, errmsg=error_map[RET.PARAMERR])
+
+    try:
+        news_id = int(news_id)
+    except Exception as e:
+        return jsonify(errno=RET.PARAMERR, errmsg=error_map[RET.PARAMERR])
+
+    if action not in ["collect", "cancel_collect"]:
+        return jsonify(errno=RET.PARAMERR, errmsg=error_map[RET.PARAMERR])
+
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg=error_map[RET.DBERR])
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg=error_map[RET.NODATA])
+
+    # 根据action执行处理（新闻收藏与用户是多对多关系，user_id和new_id建立/取消关系）
+    if action == "collect":  # 收藏
+        if news not in user.collection_news:
+            user.collection_news.append(news)
+    else:  # 取消收藏
+        if news in user.collection_news:
+            user.collection_news.remove(news)
+
+    # 返回json结果
+    return jsonify(errno=RET.OK, errmsg=error_map[RET.OK])
