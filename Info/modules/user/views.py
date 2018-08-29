7ad7f33a98243a2fb8b2c9ea0e34bd7cd9d2b586
Allcen <1310584051@qq.com>
2018-08-29 21:13:21 +0800

    显示/修改个人资料&显示/修改头像

--- Info/modules/user/views.py
+++ Info/modules/user/views.py
@@ -1,10 +1,14 @@
-from flask import g, redirect, render_template, abort
+from flask import g, redirect, render_template, abort, request, jsonify, current_app
 
 from Info.common import user_login_data
 from Info.modules.user import user_blu
 
 
 # 显示个人中心
+from Info.utils.image_storage import upload_img
+from Info.utils.response_code import RET, error_map
+
+
 @user_blu.route('/user_info')
 @user_login_data
 def user_info():
@@ -16,12 +20,63 @@ def user_info():
     return render_template("news/user.html", user=user)
 
 
-# 显示基本信息
-@user_blu.route('/base_info')
+# 显示/修改基本信息
+@user_blu.route('/base_info', methods=["GET", "POST"])
 @user_login_data
 def base_info():
     user = g.user
     if not user:
         return abort(404)
 
-    return render_template("news/user_base_info.html")
+    if request.method == "GET":
+        return render_template("news/user_base_info.html",user=user.to_dict())
+
+    # POST处理
+    # 获取参数
+    signature = request.json.get("signature")
+    nick_name = request.json.get("nick_name")
+    gender = request.json.get("gender")
+    # 校验参数
+    if not all([signature, nick_name]):
+        return jsonify(errno=RET.PARAMERR, errmsg=error_map[RET.PARAMERR])
+
+    if gender not in ["MAN", "WOMAN"]:
+        return jsonify(errno=RET.PARAMERR, errmsg=error_map[RET.PARAMERR])
+
+    # 修改模型数据
+    user.signature = signature
+    user.nick_name = nick_name
+    user.gender = gender
+
+    return jsonify(errno=RET.OK, errmsg=error_map[RET.OK])
+
+
+# 显示/修改头像
+@user_blu.route('/pic_info', methods=["GET", "POST"])
+@user_login_data
+def pic_info():
+    user = g.user
+    if not user:
+        return abort(404)
+
+    if request.method == 'GET':
+        return render_template("news/user_pic_info.html", user=user.to_dict())
+    # POST处理
+    try:
+        img_bytes = request.files.get("avatar").read()  # .save()方法可以将接受的文件存储起来,也可以直接用read()进行读取
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg=error_map[RET.PARAMERR])
+
+    # 上传文件
+    try:
+        file_name = upload_img(img_bytes)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR, errmsg=error_map[RET.THIRDERR])
+
+    # 修改用户的头像URL
+    user.avatar_url = file_name  # 在User表中的to_dict()里就已经为avatar_url添加了对应的前缀了,所以这里可以直接赋值
+
+    return jsonify(errno=RET.OK, errmsg=error_map[RET.OK], data=user.to_dict())
+
