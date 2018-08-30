c4cac4ef986817ea88c088bcb5b5944eaedc498e
Allcen <1310584051@qq.com>
2018-08-30 16:04:30 +0800

    后台登陆的状态保持

--- Info/modules/admin/views.py
+++ Info/modules/admin/views.py
@@ -1,4 +1,4 @@
-from flask import request, render_template, current_app, redirect, url_for
+from flask import request, render_template, current_app, redirect, url_for, session
 
 from Info.models import User
 from Info.modules.admin import admin_blu
@@ -8,13 +8,20 @@ from Info.modules.admin import admin_blu
 @admin_blu.route('/login', methods=["GET","POST"])
 def login():
     if request.method == "GET":
+        # 判断用户是否登陆
+        user_id = session.get("user_id")
+        is_admin = session.get("is_admin")
+
+        if user_id and is_admin:  # 免密码登陆
+            return redirect(url_for("admin.index"))
+
         # 渲染页面
         return render_template("admin/login.html")
     # POST处理
     username = request.form.get("username")
     password = request.form.get("password")
     if not all([username, password]):
-        return render_template("admin/login.html", errmsg="用户名.密码buwanzheng")
+        return render_template("admin/login.html", errmsg="用户名.密码不完整")
 
     # 判断该超级管理员是否存在
     try:
@@ -30,6 +37,10 @@ def login():
     if not user.check_password(password):
         return render_template("admin/login.html", errmsg="用户名/密码错误")
 
+    # 状态保持
+    session["user_id"] = user.id
+    session["is_admin"] = True
+
     # 跳转页面
     return redirect(url_for("admin.index"))
 
@@ -39,4 +50,14 @@ def login():
 def index():
     return render_template("admin/index.html")
 
+
+# 后台退出
+@admin_blu.route('/logout')
+def logout():
+    # 将用户信息从session中删除pop可以这是默认值,当键值对不存在时,不会报错并返回默认值
+    session.pop("user_id", None)  # 设置默认值,如果没有要删除的值也不会报错
+    session.pop("is_admin", None)
+
+    # 将结果返回
+    return redirect(url_for("admin.login"))
 
